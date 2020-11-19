16be6b56622328c6a4ee5399698710c1c0edfaee
Andrea Santu <andreasantu@gmail.com>
2020-11-19 18:55:22 +0100

    black text on uml diagrams

--- generate_diagrams.zsh
+++ generate_diagrams.zsh
@@ -9,7 +9,14 @@ echo "Generating Uml diagram files..."
 for file in source/uml/*.puml; do
     file_name=$(basename $file .puml)
     output="source/diagrams/${file_name}.png"
-    cat $file |docker run --rm -i think/plantuml -tpng > $output
+    output_tmp="source/diagrams/${file_name}.temp.png"
+    cat $file |docker run --rm -i think/plantuml -tpng > $output_tmp
+    if [ $? -eq 0 ]; then
+        cp $output_tmp $output
+    else
+        echo "error, skipping $file_name"
+    fi
+    rm $output_tmp
     echo $file_name
 done
 echo "...Done"
